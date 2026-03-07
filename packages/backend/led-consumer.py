d86fe08098b654df75e21ae0f7e19ec9ab2f9474
Wojciech Pietrowski <wojciechjerzy@gmail.com>
2026-03-07 19:17:40 +0100

    update

--- packages/backend/led-consumer.py
+++ packages/backend/led-consumer.py
@@ -1,11 +1,12 @@
 import sys
 import json
+import time
 from pi5neo import Pi5Neo
 
 neo = Pi5Neo('/dev/spidev0.0', 100, 800)
 neo.set_led_color(0, 255, 255, 255)  # pierwszy biały
 for i in range(1, 100):
-    neo.set_led_color(i, 0, 0, 0)    # reszta czarna
+    neo.set_led_color(i, 0, 0, 0)     # reszta czarna
 neo.update_strip()
 
 for raw_data in sys.stdin:
@@ -19,24 +20,20 @@ for raw_data in sys.stdin:
 
     if t == "init":
         number_of_pixels = payload.get("numberOfPixels")
-           neo = Pi5Neo('/dev/spidev0.0', number_of_pixels, 800)
+        neo = Pi5Neo('/dev/spidev0.0', number_of_pixels, 800)
 
-           for i in range(number_of_pixels):
-               neo.set_led_color(i, 255, 255, 255)
-               neo.update_strip()
-               time.sleep(0.1)
-               neo.set_led_color(i, 0, 0, 0)
+        for i in range(number_of_pixels):
+            neo.set_led_color(i, 255, 255, 255)
+            neo.update_strip()
+            time.sleep(0.1)
+            neo.set_led_color(i, 0, 0, 0)
 
-           neo.update_strip()
+        neo.update_strip()
 
-           print(json.dumps({"status": "init_ok"}), flush=True)
+        print(json.dumps({"status": "init_ok"}), flush=True)
 
     elif t == "update":
         pixels = payload.get("pixels", [])
-
-        if neo is None:
-            print(json.dumps({"status": "error", "msg": "not initialized"}), flush=True)
-            continue
 
         for i, color in enumerate(pixels):
             neo.set_led_color(i, *color)
