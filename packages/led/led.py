94fcf3fd6b0be36c23f2cd923616e558c6a6f862
Wojciech Pietrowski <wojciechjerzy@gmail.com>
2026-03-07 01:49:41 +0100

    update

--- packages/led/led.py
+++ packages/led/led.py
@@ -1,12 +1,12 @@
 from pi5neo import Pi5Neo
 
 # Initialize the Pi5Neo class with 10 LEDs and an SPI speed of 800kHz
-neo = Pi5Neo('/dev/spidev0.0', 10, 800)
+neo = Pi5Neo('/dev/spidev0.0', 3, 800)
 
 # Fill the strip with a red color
 neo.fill_strip(255, 0, 0)
 neo.update_strip()  # Commit changes to the LEDs
 
 # Set the 5th LED to blue
-neo.set_led_color(4, 0, 0, 255)
+neo.set_led_color(2, 0, 0, 255)
 neo.update_strip()
