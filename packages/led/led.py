11ed25295b90a7f22db04fd37d7e39853ba286f0
Wojciech Pietrowski <wojciechjerzy@gmail.com>
2026-03-07 01:46:25 +0100

    update

--- packages/led/led.py
+++ packages/led/led.py
@@ -1,30 +1,38 @@
 import board
-import neopixel
+import neopixel_spi as neopixel
 import time
 
-PIXEL_PIN = board.D18      # GPIO18
-NUM_PIXELS = 2
-ORDER = neopixel.GRB       # jeśli kolory będą złe, zmień na neopixel.RGB
+# Konfiguracja
+PIXEL_COUNT = 3  # Liczba diod
+PIXEL_PIN = board.SPI() # Używamy SPI
 
-pixels = neopixel.NeoPixel(
-    PIXEL_PIN,
-    NUM_PIXELS,
-    brightness=0.2,
-    auto_write=False,
-    pixel_order=ORDER
-)
+# Inicjalizacja taśmy
+pixels = neopixel.NeoPixel_SPI(PIXEL_PIN, PIXEL_COUNT, pixel_order=neopixel.GRB)
 
-# zgaś wszystko
-pixels.fill((0, 0, 0))
-
-# tylko pierwszy LED na czerwono
-pixels[0] = (255, 0, 0)
-
-# drugi LED wyłączony
-pixels[1] = (0, 0, 0)
-
-pixels.show()
-
-# zostaw stan
-while True:
+def set_color():
+    # Ustawienie kolorów (R, G, B)
+    pixels.fill((255, 0, 0)) # Czerwony
+    pixels.show()
     time.sleep(1)
+
+    pixels.fill((0, 255, 0)) # Zielony
+    pixels.show()
+    time.sleep(1)
+
+    pixels.fill((0, 0, 255)) # Niebieski
+    pixels.show()
+    time.sleep(1)
+
+    # Czyszczenie
+    pixels.fill((0, 0, 0))
+    pixels.show()
+
+if __name__ == "__main__":
+    try:
+        print("Uruchamianie testu LED... Naciśnij Ctrl+C aby wyjść.")
+        while True:
+            set_color()
+    except KeyboardInterrupt:
+        pixels.fill((0, 0, 0))
+        pixels.show()
+        print("\nZatrzymano.")
