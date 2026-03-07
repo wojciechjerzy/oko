bc270820d352e70592b810dc397df74b837df71a
Wojciech Pietrowski <wojciechjerzy@gmail.com>
2026-03-07 01:09:18 +0100

    update

--- packages/led/led.py
+++ packages/led/led.py
@@ -1,22 +1,33 @@
-import board
-import neopixel
+from rpi_ws281x import PixelStrip, Color
 import time
 
-PIXEL_PIN = board.D18
-NUM_PIXELS = 2
-ORDER = neopixel.GRB
+LED_COUNT = 2          # masz 2 diody
+LED_PIN = 18           # GPIO18
+LED_FREQ_HZ = 800000
+LED_DMA = 10
+LED_BRIGHTNESS = 64    # 0-255
+LED_INVERT = False
+LED_CHANNEL = 0
 
-pixels = neopixel.NeoPixel(
-    PIXEL_PIN,
-    NUM_PIXELS,
-    brightness=0.3,
-    auto_write=False,
-    pixel_order=ORDER
+strip = PixelStrip(
+    LED_COUNT,
+    LED_PIN,
+    LED_FREQ_HZ,
+    LED_DMA,
+    LED_INVERT,
+    LED_BRIGHTNESS,
+    LED_CHANNEL
 )
 
-pixels[0] = (255, 0, 0)  # pierwszy LED czerwony
-pixels[1] = (0, 0, 0)    # drugi LED zgaszony
-pixels.show()
+strip.begin()
+
+# pierwszy LED czerwony
+strip.setPixelColor(0, Color(255, 0, 0))
+
+# drugi LED wyłączony
+strip.setPixelColor(1, Color(0, 0, 0))
+
+strip.show()
 
 while True:
     time.sleep(1)
