a3fcdc877cff0bcab8423b1196f576f6f359a7cf
Wojciech Pietrowski <wojciechjerzy@gmail.com>
2026-03-08 19:56:31 +0100

    update

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import {info} from "./info.js";
 import {light} from "./light.js";
 import {PixelClock} from "./PixelClock.js";
+import {shutdown} from "./shutdown.js";
 
 const PORT = 2137;
 const app = express();
@@ -11,6 +12,7 @@ app.use(cors());
 
 app.get("/info", info());
 app.get("/light", light());
+app.get("/shutdown", shutdown());
 app.use(express.static("../frontend/dist"));
 
 app.listen(PORT, () => console.log(`Sleep server listening on http://localhost:${PORT}`));
---
+++ packages/backend/src/shutdown.ts
@@ -0,0 +1,22 @@
+import {Request, Response} from "express";
+import {execSync} from "child_process";
+
+export function shutdown() {
+    return (req: Request, res: Response) => {
+        const platform = process.platform;
+        if (platform === "win32") {
+            return res.json({
+                value: 100
+            });
+        } else if (platform === "darwin") {
+            return res.json({
+                value: 100
+            });
+        } else {
+            const result = execSync("sudo shutdown -h now").toString().trim();
+            return res.json({
+                value: result
+            });
+        }
+    };
+}
